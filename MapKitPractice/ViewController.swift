2c506312c2a6526d04255ed85676a16484d6404d
Aries Yang <ariesssyyyang@gmail.com>
2019-08-04 18:30:10 +0800

    New project

---
+++ MapKitPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitPractice
+//
+//  Created by Aries Yang on 2019/8/4.
+//  Copyright © 2019 Aries Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
