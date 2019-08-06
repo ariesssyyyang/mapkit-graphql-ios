6482023869746919c31720222c6e99859f4f7f85
Aries Yang <ariesssyyyang@gmail.com>
2019-08-06 17:43:50 +0800

    Implement MKMarkerAnnotationView

--- MapKitPractice/ViewController.swift
+++ MapKitPractice/ViewController.swift
@@ -25,6 +25,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.delegate = self
         centerMapOnLocation(location: initialLocation)
     }
 
@@ -75,5 +76,25 @@ class ViewController: UIViewController {
             )
         }
         return .success(restaurants)
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? Restaurant else { return nil }
+        let identifier = "restaurant"
+        var view: MKMarkerAnnotationView
+
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+
+        return view
     }
 }
